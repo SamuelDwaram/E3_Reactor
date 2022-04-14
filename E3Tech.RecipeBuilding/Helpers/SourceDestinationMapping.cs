0092dbe5715fd741dc57024c35f30ada98efd76c
SamuelDwaram <sd@e3techsys.com>
2022-04-14 15:14:33 +0530

    BugFixes: 1. Transfer mappings 2. ParameterHost V42 Pos Change

--- E3Tech.RecipeBuilding/Helpers/SourceDestinationMapping.cs
+++ E3Tech.RecipeBuilding/Helpers/SourceDestinationMapping.cs
@@ -10,18 +10,18 @@ namespace E3Tech.RecipeBuilding.Helpers
     {
         public static Dictionary<string, List<string>> SourceDestinationPair = new Dictionary<string, List<string>>()
         {
+            { "DCM", new List<string>{ "MVA 25L", "RV 50L" } },
+            { "MVB", new List<string>{  "RV 50L" } },
             { "AA1", new List<string>{ "MVA 25L", "RV 50L" } },
             { "AA2", new List<string>{ "MVA 25L", "RV 50L" } },
             { "Fill In", new List<string> { "R1", "R2", "R3", "R4","R5", "AA1", "AA2", "RV 50L", "DCM" } },
-            { "Flash", new List<string>{  "R1", "R2", "R3", "R4","R5", "AA1", "AA2", "RV 50L", "DCM", "MVA 25L" } },
+            { "Flash", new List<string>{  "MVA 25L", "RV 50L" } },
             { "MVA", new List<string>{ "RV 50L" } },
-            { "R1", new List<string>{ "R1", "R4", "R5", "AA1", "AA2", "MVB", "MVA 25L", "RV 50L" } },
+            { "R1", new List<string>{  "R3", "R4", "R5", "AA1", "AA2", "MVB", "MVA 25L", "RV 50L" } },
             { "R2", new List<string>{ "MVA 25L"} },
             { "R3", new List<string>{ "MVA 25L"} },
             { "R4", new List<string>{ "MVA 25L"} },
-            { "R5", new List<string>{ "MVB 25L"} },
-            { "MVB", new List<string>{ } },
-            { "DCM",  new List<string>{ }}
+            { "R5", new List<string>{ "MVB"} }
 
         };
     }
